4cb36e02ef218b918a3e2e81d99a327e1c05c30c
bank-builder <andrew@Turpin.co.za>
2019-03-06 21:39:25 +0200

    Update step-2.sh
    Added \pset pager off equivalent to command line

--- step-2.sh
+++ step-2.sh
@@ -2,7 +2,7 @@ echo "STEP2: ..."
 echo "'SELECT * FROM fica_status;'"
 PGPASSWORD=postgres psql -U postgres -p 9432 -h localhost -d fica_db -c 'SELECT * FROM fica_status;'
 echo "'SELECT * FROM jibar ORDER BY valid_from;'"
-PGPASSWORD=postgres psql -U postgres -p 8432 -h localhost -d jibar_db -c 'SELECT * FROM jibar ORDER BY valid_from;'
+PGPASSWORD=postgres psql -P pager=off -U postgres -p 8432 -h localhost -d jibar_db -c 'SELECT * FROM jibar ORDER BY valid_from;'
 echo "'\dt'"
 PGPASSWORD=postgres psql -P pager=off -U postgres -p 7432 -h localhost -d canonical_db -c '\dt'
 echo "Show what is in information_schema.triggers"
