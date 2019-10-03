5d3bccb7fc816fb39aff6451ef7c134680cef72d
Melissa Palmer <melissa.palmer@gmail.com>
2019-10-03 21:00:15 +0200

    fix script, don't run jibar queries

--- docker-compose/query_canonical_fica_db.sh
+++ docker-compose/query_canonical_fica_db.sh
@@ -1,9 +1,5 @@
 echo "Querying the '_fica.fica_status' table on canonical DB"
 docker run --rm -it --network=dockercompose_default postgres:10-alpine psql -h db-canonical -U postgres -p 5432 -d canonical_db -c 'SELECT * FROM _fica.fica_status;'
-echo "Querying the '_jibar.jibar' table on canonical DB"
-docker run --rm -it --network=dockercompose_default postgres:10-alpine psql -h db-canonical -U postgres -p 5432 -d canonical_db -c 'SELECT * FROM _jibar.jibar;'
 
 echo "Querying the '_fica.fica_status_history' table on canonical DB"
 docker run --rm -it --network=dockercompose_default postgres:10-alpine psql -h db-canonical -U postgres -p 5432 -d canonical_db -c 'SELECT * FROM _fica.fica_status_history;'
-echo "Querying the '_jibar.jibar_history' table on canonical DB"
-docker run --rm -it --network=dockercompose_default postgres:10-alpine psql -h db-canonical -U postgres -p 5432 -d canonical_db -c 'SELECT * FROM _jibar.jibar_history;'
