5b606bde6429e7abf0cceb74bea29b92d94608fe
Melissa Palmer <melissa.palmer@gmail.com>
2019-10-03 18:41:51 +0200

    disable table trigger on canonical db when history in ms-db too

--- docker-compose/write_flyway_migration_scripts_for_version2.sh
+++ docker-compose/write_flyway_migration_scripts_for_version2.sh
@@ -4,6 +4,6 @@ mv fica-ms/db/sql/upgrade_example/C*.sql fica-ms/db/sql/canonical/
 mv fica-ms/db/sql/upgrade_example/V*.sql fica-ms/db/sql/ms/
 mv fica-ms/db/sql/upgrade_example/D*.sql fica-ms/db/sql/ms/
 
-mv jibar-ms/db/sql/upgrade_example/C*.sql jibar-ms/db/sql/canonical/
-mv jibar-ms/db/sql/upgrade_example/V*.sql jibar-ms/db/sql/ms/
+# mv jibar-ms/db/sql/upgrade_example/C*.sql jibar-ms/db/sql/canonical/
+# mv jibar-ms/db/sql/upgrade_example/V*.sql jibar-ms/db/sql/ms/
 mv jibar-ms/db/sql/upgrade_example/D*.sql jibar-ms/db/sql/ms/
