489d4a52554b2d1cbf4512effbd0874a54bcde4b
Melissa Palmer <melissa.palmer@gmail.com>
2019-10-05 20:44:32 +0200

    remove

--- deployment-demo/jibar-ms/src/main/java/za/co/temporal/journey/model/JibarRespository.java
+++ deployment-demo/jibar-ms/src/main/java/za/co/temporal/journey/model/JibarRespository.java
@@ -1,16 +1,3 @@
-/**
- * *************************************************
- * Copyright Grindrod Bank Limited 2019, All Rights Reserved.
- * **************************************************
- * NOTICE:  All information contained herein is, and remains
- * the property of Grindrod Bank Limited.
- * The intellectual and technical concepts contained
- * herein are proprietary to Grindrod Bank Limited
- * and are protected by trade secret or copyright law.
- * Use, dissemination or reproduction of this information/material
- * is strictly forbidden unless prior written permission is obtained
- * from Grindrod Bank Limited.
- */
 package za.co.temporal.journey.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
