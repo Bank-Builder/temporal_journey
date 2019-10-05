ef23e7e81cae2fb0155ee8d6ef0f09e9ed344a7b
Melissa Palmer <melissa.palmer@gmail.com>
2019-10-05 13:30:57 +0200

    remove licence header

--- deployment-demo/bank-branches-ms/src/main/java/za/co/temporal/journey/model/FicaStatusRespository.java
+++ deployment-demo/bank-branches-ms/src/main/java/za/co/temporal/journey/model/FicaStatusRespository.java
@@ -1,10 +1,9 @@
-
 package za.co.temporal.journey.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JibarRespository extends JpaRepository<Jibar, Long> {
+public interface FicaStatusRespository extends JpaRepository<Bank, Long> {
 
 }
